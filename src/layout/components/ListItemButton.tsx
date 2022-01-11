a710a54a3ec77070c9b4642786b51ee5dc1438bf
nikdast <mahyar768@gmail.com>
2022-01-11 13:35:14 +0330

    feat: add dashboard layout and component

---
+++ src/layout/components/ListItemButton.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import Collapse from '@mui/material/Collapse';
+
+type MenuOption = {
+  title: string;
+  icon?: JSX.Element;
+  path: string;
+  subItems?: null | MenuOption[];
+};
+
+const style = {
+  selectedItem: {
+    backgroundColor: '#E3F2FD',
+    borderRadius: 1,
+    '&& svg': {
+      color: '#2196F3',
+    },
+  },
+  icon: {
+    minWidth: '35px',
+    '& svg': {
+      color: '#757575',
+    },
+  },
+};
+
+const ListItem = ({ icon, title, path, subItems }: MenuOption) => {
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+  let navigate = useNavigate();
+  const selected = location.pathname === path;
+
+  const handleClick = () => {
+    if (!!subItems) {
+      setOpen(!open);
+    } else {
+      navigate(path, { replace: true });
+    }
+  };
+
+  return (
+    <>
+      <ListItemButton sx={selected ? style.selectedItem : {}} onClick={handleClick}>
+        <ListItemIcon sx={style.icon}>{icon}</ListItemIcon>
+        <ListItemText primary={title} />
+        {!!subItems && (open ? <ExpandLess /> : <ExpandMore />)}
+      </ListItemButton>
+      {!!subItems && (
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          {subItems.map((link, i) => (
+            <Link to={link.path} key={i}>
+              <List component="a" disablePadding>
+                <ListItemButton sx={{ pl: 4 }}>
+                  <ListItemText primary={link.title} />
+                </ListItemButton>
+              </List>
+            </Link>
+          ))}
+        </Collapse>
+      )}
+    </>
+  );
+};
+export default ListItem;
---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+const Dashboard: React.ComponentType = () => {
+  return (
+    <>
+      <Box>dashboard</Box>
+    </>
+  );
+};
+
+export default Dashboard;
