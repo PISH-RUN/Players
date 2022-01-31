16a44b8540c8a987bc52017e289c9feda816fd09
Hamid Sarani <h.sarani75@gmail.com>
2022-01-31 18:52:19 +0330

    medals

--- src/components/cards/Medals.tsx
+++ src/components/cards/Medals.tsx
@@ -1,27 +1,49 @@
-import { Col, Row } from 'antd'
-import React from 'react'
-import { Card } from '../common/Card'
-import { CardCustomTitle } from '../common/CardCustomTitle'
-import { IconWithText } from '../common/IconWithText'
+import { Col, Row, Skeleton } from 'antd';
+import React, { useState } from 'react';
+import { Card } from '../common/Card';
+import { CardCustomTitle } from '../common/CardCustomTitle';
+import { IconWithText } from '../common/IconWithText';
 import SunImage from 'public/images/sun.svg';
 import P2Image from 'public/images/p2.svg';
 import RacketImage from 'public/images/racket.svg';
-
+import { useStatsList } from '../../hooks';
 
 export const Medals = (): JSX.Element => {
+  const { data, isLoading } = useStatsList();
 
-
-
-
-
+  if (isLoading) {
     return (
-        <Card>
-            <CardCustomTitle title="مدال‌های شما" />
-            <Row gutter={[0,10]}>
-                <Col xs={24}><IconWithText imgSrc={SunImage} text="۲ مدال طلای سریع‌تر از نور به ارزش ۶ کیلو طلا" /></Col>
-                <Col xs={24}><IconWithText imgSrc={P2Image} text="۲ مدال طلای سریع‌تر از نور به ارزش ۶ کیلو طلا" /></Col>
-                <Col xs={24}><IconWithText imgSrc={RacketImage} text="۲ مدال طلای سریع‌تر از نور به ارزش ۶ کیلو طلا" /></Col>
-            </Row>
-        </Card>
-    )
-}
+      <Card>
+        <Skeleton paragraph={{ rows: 0 }} />
+        <Skeleton paragraph={{ rows: 0 }} />
+        <Skeleton paragraph={{ rows: 0 }} />
+      </Card>
+    );
+  }
+
+  return (
+    <Card>
+      <CardCustomTitle title="مدال‌های شما" />
+      <Row gutter={[0, 10]}>
+        <Col xs={24}>
+          <IconWithText
+            imgSrc={SunImage}
+            text={`${data.data?.medals.light} مدال طلای سریع‌تر از نور به ارزش ۶ کیلو طلا`}
+          />
+        </Col>
+        <Col xs={24}>
+          <IconWithText
+            imgSrc={P2Image}
+            text={`${data.data?.medals.rocket} مدال طلای سریع‌تر از نور به ارزش ۶ کیلو طلا`}
+          />
+        </Col>
+        <Col xs={24}>
+          <IconWithText
+            imgSrc={RacketImage}
+            text={`${data.data?.medals.jet} مدال طلای سریع‌تر از نور به ارزش ۶ کیلو طلا`}
+          />
+        </Col>
+      </Row>
+    </Card>
+  );
+};
