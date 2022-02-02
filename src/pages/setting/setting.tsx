ed5ea28dcb4a0e12ac9f6af94dfa01352a8656c6
Hamid Sarani <h.sarani75@gmail.com>
2022-02-02 16:06:19 +0330

    fix setting

--- src/pages/setting/setting.tsx
+++ src/pages/setting/setting.tsx
@@ -41,9 +41,10 @@ const Setting: FC = () => {
       ...values,
       equipment: values.equipment?.join(','),
       abilities: values.abilities?.join(','),
-      birthdate: !!values.year && !!values.month && !!values.day ? moment(`${values.year}-${values.month}-${values.day}`, 'jYYYY-jMM-jDD')
-        .locale('en')
-        .format('YYYY-MM-DD') : undefined,
+      birthdate:
+        !!values.year && !!values.month && !!values.day
+          ? moment(`${values.year}-${values.month}-${values.day}`, 'jYYYY-jMM-jDD').locale('en').format('YYYY-MM-DD')
+          : undefined,
       day: undefined,
       month: undefined,
       year: undefined,
@@ -52,10 +53,10 @@ const Setting: FC = () => {
   };
 
   if (isLoading) {
-    return <SyncOutlined className='setting-loading' spin />;
+    return <SyncOutlined className="setting-loading" spin />;
   }
 
-  const me = participant?.data?.users_permissions_user;
+  const me = participant?.data?.users_permissions_user || {};
 
   return (
     <Form
@@ -63,34 +64,34 @@ const Setting: FC = () => {
       labelCol={{ flex: '100px' }}
       initialValues={{
         ...me,
-        equipment: me.equipment?.split(','),
-        abilities: me.abilities?.split(','),
-        day: me.birthdate ? moment.utc(new Date(me.birthdate)).format('jDD') : undefined,
-        month: me.birthdate ? moment.utc(new Date(me.birthdate)).format('jMM') : undefined,
-        year: me.birthdate ? moment.utc(new Date(me.birthdate)).format('jYYYY') : undefined,
+        equipment: me?.equipment?.split(','),
+        abilities: me?.abilities?.split(','),
+        day: me?.birthdate ? moment.utc(new Date(me.birthdate)).format('jDD') : undefined,
+        month: me?.birthdate ? moment.utc(new Date(me.birthdate)).format('jMM') : undefined,
+        year: me?.birthdate ? moment.utc(new Date(me.birthdate)).format('jYYYY') : undefined,
       }}
     >
-      <Row id='settings' gutter={[20, 20]}>
+      <Row id="settings" gutter={[20, 20]}>
         <Col span={24}>
           <PageHeader
-            className='page-header'
-            title='حساب کاربری'
+            className="page-header"
+            title="حساب کاربری"
             extra={
-              <Button htmlType='submit' type='primary' loading={isUpdateLoading}>
+              <Button htmlType="submit" type="primary" loading={isUpdateLoading}>
                 ذخیره
               </Button>
             }
           />
         </Col>
         <Col span={14}>
-          <Card title='اطلاعات شخصی' className='personal-detail card'>
-            <Form.Item label='نام' name='firstName'>
+          <Card title="اطلاعات شخصی" className="personal-detail card">
+            <Form.Item label="نام" name="firstName">
               <Input />
             </Form.Item>
-            <Form.Item label='نام خانوادگی' name='lastName'>
+            <Form.Item label="نام خانوادگی" name="lastName">
               <Input />
             </Form.Item>
-            <Form.Item label='جنسیت' name='gender'>
+            <Form.Item label="جنسیت" name="gender">
               <Radio.Group
                 options={[
                   {
@@ -104,7 +105,7 @@ const Setting: FC = () => {
                 ]}
               />
             </Form.Item>
-            <Form.Item label='وضعیت تاهل' name='maritalStatus'>
+            <Form.Item label="وضعیت تاهل" name="maritalStatus">
               <Radio.Group
                 options={[
                   {
@@ -118,48 +119,48 @@ const Setting: FC = () => {
                 ]}
               />
             </Form.Item>
-            <Form.Item label='تعداد فرزند' name='children'>
+            <Form.Item label="تعداد فرزند" name="children">
               <InputNumber />
             </Form.Item>
-            <Form.Item label='آدرس ایمیل' name='email'>
+            <Form.Item label="آدرس ایمیل" name="email">
               <Input />
             </Form.Item>
-            <Form.Item label='تاریخ تولد'>
+            <Form.Item label="تاریخ تولد">
               <Input.Group compact>
-                <Form.Item name='day' noStyle>
-                  <Input style={{ width: 40 }} placeholder='روز' />
+                <Form.Item name="day" noStyle>
+                  <Input style={{ width: 40 }} placeholder="روز" />
                 </Form.Item>
-                <Form.Item name='month' noStyle>
-                  <Input style={{ width: 40 }} placeholder='ماه' />
+                <Form.Item name="month" noStyle>
+                  <Input style={{ width: 40 }} placeholder="ماه" />
                 </Form.Item>
-                <Form.Item name='year' noStyle>
-                  <Input style={{ width: 60 }} placeholder='سال' />
+                <Form.Item name="year" noStyle>
+                  <Input style={{ width: 60 }} placeholder="سال" />
                 </Form.Item>
               </Input.Group>
             </Form.Item>
-            <Form.Item label='مدرک تحصیلی' name='degree'>
+            <Form.Item label="مدرک تحصیلی" name="degree">
               <Input />
             </Form.Item>
-            <Form.Item label='رشته تحصیلی' name='fieldStudy'>
+            <Form.Item label="رشته تحصیلی" name="fieldStudy">
               <Input />
             </Form.Item>
           </Card>
         </Col>
         <Col span={10}>
           <Row>
-            <Card title='تجهیزات شخصی' className='card'>
-              <Form.Item name='equipment' initialValue={me.equipment && me.equipment.split(',')}>
+            <Card title="تجهیزات شخصی" className="card">
+              <Form.Item name="equipment" initialValue={me.equipment && me.equipment.split(',')}>
                 <Checkbox.Group>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='لپ تاپ'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="لپ تاپ">
                     لپ تاپ
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='گوشی هوشمند'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="گوشی هوشمند">
                     گوشی هوشمند
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='تبلت'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="تبلت">
                     تبلت
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='اینترنت'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="اینترنت">
                     اینترنت
                   </Checkbox>
                 </Checkbox.Group>
@@ -168,40 +169,40 @@ const Setting: FC = () => {
           </Row>
           <br />
           <Row>
-            <Card title='توانمندی ها' className='card'>
-              <Form.Item name='abilities' initialValue={me.abilities && me.abilities.split(',')}>
+            <Card title="توانمندی ها" className="card">
+              <Form.Item name="abilities" initialValue={me.abilities && me.abilities.split(',')}>
                 <Checkbox.Group>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='لپ گرافیک'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="لپ گرافیک">
                     گرافیک
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='برنامه نویسی'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="برنامه نویسی">
                     برنامه نویسی
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='تولید و تدوین محتوای ویدیویی'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="تولید و تدوین محتوای ویدیویی">
                     تولید و تدوین محتوای ویدیویی
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='تولید و تدوین محتوای صوتی'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="تولید و تدوین محتوای صوتی">
                     تولید و تدوین محتوای صوتی
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='آشنایی با شبکه های اجتماعی'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="آشنایی با شبکه های اجتماعی">
                     آشنایی با شبکه های اجتماعی
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='نگارش متن و تولید محتوا'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="نگارش متن و تولید محتوا">
                     نگارش متن و تولید محتوا
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='مدیریت کسب و کار'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="مدیریت کسب و کار">
                     مدیریت کسب و کار
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='بازاریابی و تبلیفات'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="بازاریابی و تبلیفات">
                     بازاریابی و تبلیفات
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='تعلیم و تربیت'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="تعلیم و تربیت">
                     تعلیم و تربیت
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='آموزش و پژوهش'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="آموزش و پژوهش">
                     آموزش و پژوهش
                   </Checkbox>
-                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value='کسب و کار'>
+                  <Checkbox style={{ width: '100%', marginBottom: 10 }} value="کسب و کار">
                     کسب و کار
                   </Checkbox>
                 </Checkbox.Group>
