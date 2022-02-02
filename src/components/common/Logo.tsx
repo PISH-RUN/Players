f220b9687e075225c0d8cec19910a8176a1cf8db
Hamid Sarani <h.sarani75@gmail.com>
2022-02-02 18:57:56 +0330

    fix favicon

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { ImageBox } from "./ImageBox";
-import { Title, Type } from "./Title";
-import './styles/Logo.less'
+import React from 'react';
+import { ImageBox } from './ImageBox';
+import { Title, Type } from './Title';
+import './styles/Logo.less';
 import LogoIcon from 'public/images/pishrun-logo.png';
 
-export const Logo = (props:{subject?:boolean}): JSX.Element => {
+export const Logo = (props: { subject?: boolean }): JSX.Element => {
+  const { subject } = props;
 
-    const {subject} = props
-
-    return (
-        <div className="logo-title">
-            <ImageBox src={LogoIcon} width="38" height="38" />
-            {subject && <Title className="site-name" type={Type.THIN}> پیش‌ران</Title>}
-        </div>
-    )
-}
+  return (
+    <div className="logo-title">
+      <ImageBox src={LogoIcon} width="38" height="38" />
+      {subject && (
+        <Title className="site-name" type={Type.THIN}>
+          {' '}
+          پیش‌ران
+        </Title>
+      )}
+    </div>
+  );
+};
